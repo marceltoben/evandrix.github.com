656796cc93b5224cbc05479afdd0150c9284dcb4
Lee Wei <evandrix@gmail.com>
2011-07-11 00:54:15 +0100

    added another obsfucated c word to number translator (with usage instructions inside)

---
+++ c/spokennum-long.c
@@ -0,0 +1,5 @@
+/* gcc -w -o spokennum spokennum-long.c && echo "a thousand" | ./spokennum; echo */
+long long u,v;main(a,b,c,d){for(b=c=0;c++||(a=getchar()|32)>46;b
+="ynwtsflrabg"[c]-a?b:b*8+c)c%=11;for(c=b%64-25;b;b/=8)for(d=19;
+d;"1+DIY/.K430x9G(kC["[d]-42&255^b||(v+=d>15?d:d>9?v%u*~-u:c+1?d
++!c*16:d*16,b=0))u=1ll<<6177%d--*4;a<0?printf("%llx",v):main();}
